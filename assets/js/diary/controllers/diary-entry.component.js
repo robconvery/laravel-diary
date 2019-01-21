a8672c2442bbca37e9e53bdeeb894a4e989f5db1
Robert Convery <robconvery@me.com>
2019-01-21 21:31:34 +0000

    Created  bespoke `diary entry` component

--- assets/js/diary/controllers/diary-entry.component.js
+++ assets/js/diary/controllers/diary-entry.component.js
@@ -1,8 +1,11 @@
 
 angular.module('diaryEntryCtrl', []).
-    component('diaryEntry', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
+    component('diaryEntry', {
         template: '', // change to suite
         controller: function DiaryEntryController() {
-            console.log('data entry controller, change to suite');
-        }
+            /*console.log('data entry controller, change to suite');*/
+        },
+    bindings: {
+        entry: '<'
+    }
     });
